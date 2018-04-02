6930f740b79dda229539040d16560ceaf4233744
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2018-04-02 12:39:51 +0900

    implements react app base

---
+++ app/js/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Button from "material-ui/Button";
+
+const App = () => (
+  <Button
+    variant="raised"
+    color="primary"
+  >
+    Hello World
+  </Button>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const {resolve} = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: "app/index.html",
+  }),
+];
+
+module.exports = {
+  mode: "development",
+
+  entry: resolve(__dirname, "app/js/index.tsx"),
+  output: {
+    filename: "bundle.js",
+    path: resolve(__dirname, "dist")
+  },
+  plugins: plugins,
+
+  devtool: "source-map",
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      {test: /\.tsx?$/, loader: "ts-loader"},
+      {enforce: "pre", test: /\.js$/, loader: "source-map-loader"},
+    ]
+  },
+};
