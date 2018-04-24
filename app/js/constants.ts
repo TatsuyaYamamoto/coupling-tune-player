7127e73899389791131abcbe0ee83f76fe4cb344
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2018-04-24 23:02:22 +0900

    fix ,invalid text resource

--- app/js/constants.ts
+++ app/js/constants.ts
@@ -2,7 +2,7 @@
  * External URLs.
  */
 export const URL = {
-  COUPLING_TUNE_PLAYER: "http://apps.sokontokoro-factory.net/coupling-tune-player/",
+  COUPLING_TUNE_PLAYER: "https://apps.sokontokoro-factory.net/coupling-tune-player/",
   SOKONTOKORO_HOME: "https://www.sokontokoro-factory.net",
   TWITTER_TWEET_PAGE: "https://twitter.com/intent/tweet",
 };
