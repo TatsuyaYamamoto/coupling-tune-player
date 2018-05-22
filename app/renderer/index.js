3efb08611dc74d47176a8e2d125418e373280aae
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2018-05-22 20:47:40 +0900

    delete electron starter files

--- app/renderer/index.js
+++
@@ -1,9 +0,0 @@
-const SyncAudioPlayer = require("./SyncAudioPlayer");
-
-
-const player = new SyncAudioPlayer({
-    leftAudioSrc: '/Users/fx30328/Desktop/AQUARIUM.mp3',
-    rightAudioSrc: '/Users/fx30328/Desktop/AQUARIUM.mp3',
-});
-
-player.play();
