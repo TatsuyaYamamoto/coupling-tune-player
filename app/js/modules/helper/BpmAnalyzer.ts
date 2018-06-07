01287c3e98985c27ffaf76455ad1efa5605c1c58
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2018-06-07 21:19:05 +0900

    use left channel only in analyzing

--- app/js/modules/helper/BpmAnalyzer.ts
+++ app/js/modules/helper/BpmAnalyzer.ts
@@ -14,89 +14,78 @@ export interface AnalyzeResult {
  * 計算結果の位相の都合上、startPositionは負の値を取ることもあるので、注意
  *
  * @param {AudioBuffer} audio
- * @returns {Promise<AnalyzeResult>}
+ * @returns {AnalyzeResult}
  */
-export async function analyzeBpm(audio: AudioBuffer): Promise<AnalyzeResult> {
-  const {
-    duration,
-    length,
-    numberOfChannels,
-    sampleRate: samplingRate
-  } = audio;
+export function analyzeBpm(audio: AudioBuffer): AnalyzeResult {
+  const { length: samplingCount, sampleRate: samplingRate } = audio;
 
-  const channels = [...Array(numberOfChannels)]
-    .fill(0)
-    .map((_, i) => audio.getChannelData(i));
+  const channel = audio.getChannelData(0);
 
-  const results: AnalyzeResult[] = channels.map((channel: Float32Array) => {
-    const N = floor(channel.length / SAMPLES_PER_FRAME);
+  const N = floor(samplingCount / SAMPLES_PER_FRAME);
 
-    const effectiveVolumes = [...Array(N)].fill(0).map((v, i) => {
-      const startIndex = i * SAMPLES_PER_FRAME;
+  const effectiveVolumes: number[] = [];
+  // tslint:disable-next-line
+  for (let i = 0; i < N; i++) {
+    const startIndex = i * SAMPLES_PER_FRAME;
+    const endIndex = startIndex + SAMPLES_PER_FRAME;
 
-      return effectiveValueOf(
-        channel.slice(startIndex, startIndex + SAMPLES_PER_FRAME)
-      );
-    });
+    effectiveVolumes.push(
+      effectiveValueOf(channel.slice(startIndex, endIndex))
+    );
+  }
 
-    const volumeDiffs = effectiveVolumes.map((value, index) => {
-      const diff = effectiveVolumes[index + 1] - effectiveVolumes[index];
+  const volumeDiffs = effectiveVolumes.map((value, index) => {
+    const diff = effectiveVolumes[index + 1] - effectiveVolumes[index];
 
-      return diff > 0 ? diff : 0;
-    });
-
-    const a = [];
-    const b = [];
-    const r = [];
-    const framesPerSec = samplingRate / SAMPLES_PER_FRAME;
-
-    // tslint:disable-next-line:no-increment-decrement
-    for (let i /*BPM*/ = MIN_BPM; i <= MAX_BPM; i++) {
-      let aSum = 0;
-      let bSum = 0;
-      const beatsPerSec = i / 60;
-      const beatsPerFrame = beatsPerSec / framesPerSec;
-
-      volumeDiffs.forEach((diff, j) => {
-        const win = hannWindow(j / N);
-
-        aSum += diff * cos(2 * PI * beatsPerFrame * j) * win;
-        bSum += diff * sin(2 * PI * beatsPerFrame * j) * win;
-      });
-
-      const aBpm = aSum / N;
-      const bBpm = bSum / N;
-
-      a[i - MIN_BPM] = aBpm;
-      b[i - MIN_BPM] = bBpm;
-      r[i - MIN_BPM] = sqrt(aBpm * aBpm + bBpm * bBpm);
-    }
-
-    let bpm = 0;
-    let maxAmp = 0;
-
-    r.forEach((value, index) => {
-      if (maxAmp < value) {
-        maxAmp = value;
-        bpm = index;
-      }
-    });
-
-    bpm += MIN_BPM;
-
-    // 位相差
-    const theta = atan2(b[bpm - MIN_BPM], a[bpm - MIN_BPM]);
-    const startPosition = theta / (2 * PI * bpm / 60);
-
-    return {
-      bpm,
-      startPosition
-    };
+    return diff > 0 ? diff : 0;
   });
 
+  const a = [];
+  const b = [];
+  const r = [];
+  const framesPerSec = samplingRate / SAMPLES_PER_FRAME;
+
+  // tslint:disable-next-line:no-increment-decrement
+  for (let i /*BPM*/ = MIN_BPM; i <= MAX_BPM; i++) {
+    let aSum = 0;
+    let bSum = 0;
+    const beatsPerSec = i / 60;
+    const beatsPerFrame = beatsPerSec / framesPerSec;
+
+    volumeDiffs.forEach((diff, j) => {
+      const win = hannWindow(j / N);
+
+      aSum += diff * cos(2 * PI * beatsPerFrame * j) * win;
+      bSum += diff * sin(2 * PI * beatsPerFrame * j) * win;
+    });
+
+    const aBpm = aSum / N;
+    const bBpm = bSum / N;
+
+    a[i - MIN_BPM] = aBpm;
+    b[i - MIN_BPM] = bBpm;
+    r[i - MIN_BPM] = sqrt(aBpm * aBpm + bBpm * bBpm);
+  }
+
+  let bpm = 0;
+  let maxAmp = 0;
+
+  r.forEach((value, index) => {
+    if (maxAmp < value) {
+      maxAmp = value;
+      bpm = index;
+    }
+  });
+
+  bpm += MIN_BPM;
+
+  // 位相差
+  const theta = atan2(b[bpm - MIN_BPM], a[bpm - MIN_BPM]);
+  const startPosition = theta / (2 * PI * bpm / 60);
+
   return {
-    bpm: results[0].bpm,
-    startPosition: results[0].startPosition
+    bpm,
+    startPosition
   };
 }
 
