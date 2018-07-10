dcf7961ae871f468aad4c274fd0fd77056c7b294
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2018-07-11 00:36:14 +0900

    generate tweet text for coupling name

--- app/js/containers/organisms/AppBar.tsx
+++ app/js/containers/organisms/AppBar.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { connect } from "react-redux";
+
 import { default as styled } from "styled-components";
 import { default as AutoBind } from "autobind-decorator";
 
@@ -9,8 +11,16 @@ import InfoDialog from "../../components/molecules/InfoDialog";
 import TwitterLogoSvg from "../../components/atoms/icon/TwitterLogoSvg";
 import InfoIcon from "../../components/atoms/icon/InfoIcon";
 
-import { sendEvent, tweetByWebIntent } from "../../utils";
+import {
+  getLongestCommonSubstring,
+  sendEvent,
+  tweetByWebIntent
+} from "../../utils";
 import { URL } from "../../constants";
+import Song from "../../redux/model/Song";
+import { States } from "../../redux/store";
+import { findIdols } from "../../helper/idol/IdolUtil";
+import { createCouplingName } from "../../helper/idol/Muse";
 
 const Root = styled.div`
   display: flex;
@@ -26,8 +36,11 @@ interface ComponentState {
   isInfoDialogOpen: boolean;
 }
 
+type P = ComponentProps & StateProps;
+type S = ComponentState;
+
 @AutoBind
-class AppBar extends React.Component<ComponentProps, ComponentState> {
+class AppBar extends React.Component<P, S> {
   public state = {
     isInfoDialogOpen: false
   };
@@ -85,9 +98,32 @@ class AppBar extends React.Component<ComponentProps, ComponentState> {
   }
 
   private onShowTweet() {
+    const { left, right } = this.props;
+
+    let text = "かぷちゅうプレイヤー/Coupling Tune Player\n\n";
+
+    if (left && right) {
+      const leftTitle = left.title;
+      const leftArtist = left.artist;
+      const rightTitle = right.title;
+      const rightArtist = right.artist;
+
+      if (leftArtist && rightArtist) {
+        const leftIdol = findIdols(leftArtist)[0];
+        const rightIdol = findIdols(rightArtist)[0];
+
+        const title = getLongestCommonSubstring(leftTitle, rightTitle);
+        const couplingName = createCouplingName(leftIdol, rightIdol);
+
+        if (couplingName && title) {
+          text = `「${couplingName}」で「${title}」をかぷ中〜♪\n\n`;
+        }
+      }
+    }
+
     tweetByWebIntent({
+      text,
       url: URL.COUPLING_TUNE_PLAYER,
-      text: "かぷちゅうプレイヤー/Coupling Tune Player",
       hashtags: ["かぷちゅうプレイヤー", "そこんところ工房"]
     });
 
@@ -98,4 +134,20 @@ class AppBar extends React.Component<ComponentProps, ComponentState> {
   }
 }
 
-export default AppBar;
+interface StateProps {
+  left: Song | null;
+  right: Song | null;
+}
+
+function mapStateToProps(state: States, ownProps: ComponentProps): StateProps {
+  const { list, focusIndex, loading } = state.tracklist;
+  const left = focusIndex ? list.get(focusIndex).left : null;
+  const right = focusIndex ? list.get(focusIndex).right : null;
+
+  return {
+    left,
+    right
+  };
+}
+
+export default connect(mapStateToProps)(AppBar);
