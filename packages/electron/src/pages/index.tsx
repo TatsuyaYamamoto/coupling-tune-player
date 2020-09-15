bdda27419bf5bd6e891a62f0404c1c89f77ff136
Tatsuya Yamamoto <t28.is.tatsuya@gmail.com>
2020-09-15 17:38:40 +0900

    chore(share): create share package build env

--- packages/electron/src/pages/index.tsx
+++ packages/electron/src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { PlayerController } from "@coupling-tune-player/share";
 
 const IndexPage = () => {
   return (
     <div>
       <h1>React Electron App</h1>
       <p>Welcome to your Electron application.</p>
+      <PlayerController />
     </div>
   );
 };
---
+++ packages/share/src/components/PlayerController/PlayerController.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export const PlayerController: React.FC = () => {
+  return (
+    <div>
+      <div>HOGE!</div>
+    </div>
+  );
+};
