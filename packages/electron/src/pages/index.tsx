77ddda21696be5f19aeab8c5776eaa294246b7c0
Tatsuya Yamamoto <t28.is.tatsuya@gmail.com>
2020-09-16 01:30:47 +0900

    feat(electron): show menu drawer

--- packages/electron/src/pages/index.tsx
+++ packages/electron/src/pages/index.tsx
@@ -4,20 +4,37 @@ import React, { FC, useEffect } from "react";
 import { PlayerController } from "@coupling-tune-player/share";
 
 import useElectronMainProcess from "../components/hooks/useElectronMainProcess";
+import Drawer from "../components/organisms/Drawer";
 
 const IndexPage: FC = () => {
   const { openFileSelectDialog } = useElectronMainProcess();
 
+  const onClickMenu = () => {};
+
   return (
     <div>
-      <h1>React Electron App</h1>
-      <p>Welcome to your Electron application.</p>
-      <button onClick={() => openFileSelectDialog()}>open</button>
+      <div
+        css={css`
+          display: flex;
+        `}
+      >
+        <Drawer onClickMenu={onClickMenu} />
+        <main
+          css={css`
+            flex-grow: 1;
+          `}
+        >
+          <h1>React Electron App</h1>
+          <p>Welcome to your Electron application.</p>
+          <button onClick={() => openFileSelectDialog()}>open</button>
+        </main>
+      </div>
       <PlayerController
         css={css`
           position: fixed;
           bottom: 0;
           width: 100%;
+          z-index: 9999;
         `}
         playerState={"playing"}
         duration={3}
